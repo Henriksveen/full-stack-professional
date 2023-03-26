7fec3187073fa8865e9f72b90af96922d26768d9
henriksveen <simen.henriksveen@gmail.com>
2023-03-26 21:37:23 +0200

    Add slack and counter to PingPong

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingPongController {
-    record PingPong(String result) {
 
+    private static int COUNTER = 0;
+
+    record PingPong(String result) {
     }
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("pong");
+        return new PingPong("Pong: %s".formatted(++COUNTER));
     }
 }
