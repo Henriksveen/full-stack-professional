0e4ce042afa84d3d563ff9327afa5a33c8099233
henriksveen <simen.henriksveen@gmail.com>
2023-03-28 23:29:08 +0200

    CORS, Axios, Fetch customers & Card Component

---
+++ frontend/react/src/services/client.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const getCustomers = async () => {
+    try {
+        return await axios.get(`${import.meta.env.VITE_API_BASEURL}/api/v1/customers`);
+    } catch (err) {
+        throw err;
+    }
+}
